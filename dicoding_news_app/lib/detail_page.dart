be1602d790a1b2ccddc2e2826970661393934c5d
novannp <novangarut@gmail.com>
2021-10-28 08:04:14 +0700

    update

---
+++ dicoding_news_app/lib/detail_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'article.dart';
+
+class ArticleDetailPage extends StatelessWidget {
+  static const routeName = '/article_detail';
+
+  final Article article;
+  const ArticleDetailPage({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.urlToImage),
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            Column(
+              children: [
+                Text(
+                  article.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                Text('Date: ${article.publishedAt}'),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Author: ${article.publishedAt}'),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                Text(
+                  article.content,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
